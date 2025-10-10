b921256fd8924e95c79394a825a540f110f32574
cesarbdeveloper <tatu221004@gmail.com>
2025-10-10 13:34:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function iniciarOrden() {
 
   // Simular 15% de probabilidad de error
   const probabilidadError = Math.random();
-  if (probabilidadError < 0.7) {
+  if (probabilidadError < 0.3) {
     log.innerHTML = `<p style="color: red;"><strong>❌ Ocurrió un error. Inicie su pedido nuevamente.</strong></p>`;
     console.error("❌ Error en la orden. Reinicie.");
     return;
